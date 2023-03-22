14943bd13cd926780091f0df8e82d9628a587d50
ChirkovMihail <mlhailchirk@gmail.com>
2023-03-22 13:15:13 +0300

    add/init head and class solution_stud

---
+++ Project_git_lesson_22_03_23/git_lesson_22_03_23/Head.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+string Folder_base_put = "C://Users/User/...";
+string File_contests = "contest.txt";
+string File_compiler = "compiler.txt";
+string File_tasks = "tasks.txt";
+string File_students = "students.txt";
+string Folder_base_stud = "stud_work/";
+string Nick_name_coach = "DarkSun";
+
+const int const_levenstein = 40;
+const int const_length_code = 5000;
+const int const_time = 4;
